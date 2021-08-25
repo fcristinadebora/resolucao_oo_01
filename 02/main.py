50177aa41529dd7f566603516c1b59d479337191
Debora Cristina <fcristinadebora@gmail.com>
2021-08-25 19:38:32 -0300

    adiciona verificacao de tamanho minimo do vetor valores em exercicio 02

--- 02/main.py
+++ 02/main.py
@@ -3,5 +3,7 @@ from operacao import Operacao
 vetor_valores = [ 1.2, 2.5, 3, 6, 8 ]
 
 total = Operacao.soma(vetor_valores)
-
 print('Soma de todos os valores do vetor = ' + str(total))
+
+total_vazio = Operacao.soma([])
+print('Soma de todos os valores do vetor vazio = ' + str(total_vazio))
--- 02/operacao.py
+++ 02/operacao.py
@@ -1,6 +1,9 @@
 class Operacao:
     @staticmethod
     def soma(vetor_valores):
+        if len(vetor_valores) < 1:
+            return None
+        
         total = 0
         for valor in vetor_valores:
             total = total + valor
